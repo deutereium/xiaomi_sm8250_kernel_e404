e75ef4119b98bdf76ae33b4ed31f273840fdf18a
deu <fawwazzuladhim700@gmail.com>
2026-08-10 06:24:34 +0700

    syscall: drop redundant current_uid()
    current_uid().val already cached into cur_uid. just reuse the value
    without re calling it.
    
    Signed-off-by: deu <fawwazzuladhim700@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1247,7 +1247,7 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (current_uid().val == 0 &&
+	if (cur_uid == 0 &&
 		(!strncmp(current->comm, "bpfloader", 9) ||
 		!strncmp(current->comm, "netbpfload", 10) ||
 		!strncmp(current->comm, "uprobestats", 11) ||
